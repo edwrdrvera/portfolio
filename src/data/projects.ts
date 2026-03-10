4b3662d04b4a0e46f825a13f69c717c851c2ea19
ed <edwardr1213@gmail.com>
2026-03-10 08:43:54 -0500

    refactor: added more projects.

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -19,17 +19,39 @@ export const projects = [
       "Patient and clinic management"
     ],
     tech: ["React", "Node.js", "Tailwind"],
-    link: "#"
+    link: "https://github.com/alcoranpaul/devhacks2026"
   },
   {
     title: "Tastebuds",
-    description: "A social media application designed for sharing, discovering, and categorizing authentic restaurant reviews and recipes.",
+    description: "A social media app designed for sharing, discovering, and categorizing authentic restaurant reviews and recipes.",
     features: [
       "Restaurant feed with search and filters",
       "User profiles with reviews and recipes",
       "Restaurant and recipe bookmarking"
     ],
     tech: ["React", "Tailwind", "Express", "Neon", "Prisma"],
-    link: "#"
+    link: "https://github.com/mlim44/Tastebuds"
+  },
+  {
+    title: "Portfolio Website",
+    description: "My portfolio website for showcasing projects and skills.",
+    features: [
+      "Responsive design for optimal viewing on all devices",
+      "Clean and modern user interface",
+      "Easy navigation between projects and skills"
+    ],
+    tech: ["React", "Tailwind", "Vercel"],
+    link: "https://github.com/edwrdrvera/portfolio"
+  },
+  {
+    title: "PokeLog API",
+    description: "A comprehensive API for managing Pokémon data.",
+    features: [
+      "RESTful API for Pokémon data management",
+      "User authentication and authorization",
+      "Pokémon data management"
+    ],
+    tech: ["Node.js", "Express", "Firestore", "Vercel" ],
+    link: "https://github.com/edwrdrvera/pokelog-api"
   }
 ];
